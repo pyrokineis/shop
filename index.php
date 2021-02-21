c48d1d145765494c4fb21802a0a11807070edeb9
pyrokineis <63732622+pyrokineis@users.noreply.github.com>
2021-02-21 14:06:03 +0300

    start

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<html>
+<head>
+    <title>Бля</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+<header>
+    <div class="h1-block">
+        <h1>Продается мать</h1>
+    </div>
+    <div class="panel-block" title="Иконки">
+        <div class="panel">
+            <a>Корзина</a>
+            <a>Личный кабинет</a>
+        </div>
+    </div>
+
+
+    <nav>
+        <ul class="upper-menu">
+            <li class="upper-menu-item">Каталог</li>
+            <li class="upper-menu-item">Оплата и доставка</li>
+            <li class="upper-menu-item">Акции</li>
+            <li class="upper-menu-item">хм</li>
+            <li class="upper-menu-item">хмм</li>
+        </ul>
+
+    </nav>
+</header>
+
+<main>
+    <div class="search-container">
+        <div class="search-panel">
+            <input type="text" name="search" placeholder="Поиск на сайте">
+        </div>
+        <div class="search-button">
+            <button type="submit">Найти</button>
+        </div>
+
+    </div>
+    <div class="left-menu">
+
+    </div>
+</main>
+
+<footer>
+
+</footer>
+
+</body>
+</html>
