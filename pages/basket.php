40ed452102fb9fde5799aa2786fd596340c092b8
pyrokineis <63732622+pyrokineis@users.noreply.github.com>
2021-04-26 14:58:31 +0300

    ver 13.1 (beta) TODO cookies

--- pages/basket.php
+++ pages/basket.php
@@ -14,25 +14,43 @@
 
 <main>
     <?php include_once("../include/login_win.php");?>
+
     <div class="basket-body">
         <p class="basket-header">Корзина</p>
         <div class="basket-main">
-            <div class="basket-product-block">
-                <div class="basket-product-info">
-                    <img alt="Video for Icon" src="../pics/'.$row['category'].'/'.$row['pic'].'">
-<!--                    <p class="product-name">'.$row['naming'].'</p>-->
-                    <p class="basket-product-name">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, laborum?</p>
-<!--                    <p class="product-price">'.$row['price'].' р.<button >Добавить</button> </p>-->
-                    <p class="basket-product-price">99999 р.</p>
-                </div>
-                <div class="basket-product-options">
 
-                </div>
-            </div>
+                <?php
+                $basket_id = 2;
+                if (isset($basket_id))
+                {
+                    $query = "select * from basket_content_tbl where basket_id='$basket_id'";
+                    $basket_content_result = mysqli_query($link,$query) or die("Ошибка1" . mysqli_error($link));
+                    while ($row1=mysqli_fetch_array($basket_content_result))
+                    {
+                        $product_id = $row1['product_id'];
+                        $query = "SELECT * FROM product_tbl where id=".$product_id;
+                        $products_basket_result = mysqli_query($link, $query) or die("Ошибка2" . mysqli_error($link));
+                        while ($row2=mysqli_fetch_array($products_basket_result))
+                        {
+                            echo '
+                        <div class="basket-product-block">
+                            <div class="basket-product-info">
+                                <img alt="Video for Icon" src="../pics/'.$row2['category'].'/'.$row2['pic'].'">
+                                <p class="basket-product-name">'.$row2['naming'].'</p>
+                                <p class="basket-product-price"">'.$row2['price'].' р.<button >Добавить</button> </p>
+                            </div>
+                            <div class="basket-product-options">
+    
+                            </div>
+                        </div>
+                        ';
+                        }
+                    }
+                }
+                ?>
+
         </div>
-
-
-    </div>
+       </div>
 </main>
 
 
